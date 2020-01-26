46a3b087b4a411d56a9eb14c6bf0a6fcf27c1e9e
fernandoRodrigues29 <fernando1587@gmail.com>
2020-01-26 13:32:30 -0300

    26-01-2020

---
+++ servidor.php
@@ -0,0 +1,64 @@
+<?php
+$serv = array(
+    array(
+        "id"=>"1",
+        "nome"=>"Fernando",
+        "email"=>"a@email.com",
+        "tel"=>"5454-3587"
+    ),
+    array(
+        "id"=>"2",
+        "nome"=>"Rodrigo",
+        "email"=>"b@email.com",
+        "tel"=>"7454-3587"
+    ),
+    array(
+        "id"=>"3",
+        "nome"=>"Silvano",
+        "email"=>"c@email.com",
+        "tel"=>"8454-3587"        
+    ),
+    array(
+        "id"=>"4",
+        "nome"=>"Artur",
+        "email"=>"c@email.com",
+        "tel"=>"98454-3587"
+        
+    ),
+    array(
+        "id"=>"5",
+        "nome"=>"Gerson",
+        "email"=>"c@email.com",
+        "tel"=>"58454-3587"
+        
+    ),
+    array(
+        "id"=>"6",
+        "nome"=>"Cleber",
+        "email"=>"tc@email.com",
+        "tel"=>"68454-3587"
+        
+    ),
+    array(
+        "id"=>"7",
+        "nome"=>"Paulo",
+        "email"=>"ec@email.com",
+        "tel"=>"98454-3587"
+        
+    ),
+    array(
+        "id"=>"8",
+        "nome"=>"Trajano",
+        "email"=>"yt@email.com",
+        "tel"=>"80454-3587"
+        
+    ),
+    array(
+        "id"=>"9",
+        "nome"=>"Pericles",
+        "email"=>"wc@email.com",
+        "tel"=>"28454-3587"
+    )    
+);
+
+echo json_encode($serv);
